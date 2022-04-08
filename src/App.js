2c4bc6d1486a3386f12d5770af07c9b1af6dba84
ychi lu <luyege+github@gmail.com>
2022-04-08 20:29:05 +0000

    n

--- src/App.js
+++ src/App.js
@@ -8,14 +8,44 @@ function Todo({ todo }) {
     </div>
   );
 };
+// ...
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+
+
+// ...
 function App() {
+
   const [todos, setTodos] = React.useState([
     { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
     { text: "Build really cool todo app" }
   ]);
 
+ const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
   return (
     <div className="app">
       <div className="todo-list">
@@ -26,6 +56,7 @@ function App() {
             todo={todo}
           />
         ))}
+          <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
