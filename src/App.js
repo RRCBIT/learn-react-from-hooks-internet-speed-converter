efaaae21e2328371f1767784a229acda12e2ad00
ychi lu <luyege+github@gmail.com>
2022-04-08 16:06:27 +0000

    fwef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function Todo({ todo }) {
   return (
@@ -11,7 +10,25 @@ function Todo({ todo }) {
 };
 
 function App() {
-  // ...
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
